75ff6aba0a6a10bb37783c6d78153348ec8d3e28
Oladejo Sunday <sundayoladejo13@gmail.com>
2021-07-25 17:30:18 +0100

    fix app js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import axios from 'axios';
 
 
 function App() {
-  const fetchData = async () =>{
-    try{
-      const response = await axios.get('http://localhost:8888/api/midraycars');
-      // console.log(response)
-    }
-    catch(error){
-      console.log(error.response)
-    }
+  // const fetchData = async () =>{
+  //   try{
+  //     const response = await axios.get('http://localhost:8888/api/midraycars');
+  //     // console.log(response)
+  //   }
+  //   catch(error){
+  //     console.log(error.response)
+  //   }
    
-  }
+  // }
 
-  fetchData();
+  // fetchData();
 
   return (
     <div className="App">
