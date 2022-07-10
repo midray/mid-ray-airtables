772f71d64b950b75c03bee2dc1fc5f71cbea2a04
Abideen <ogunrinuabideen@gmail.com>
2022-07-10 08:31:59 +0100

    single product

--- functions/purfashion.js
+++ functions/purfashion.js
@@ -11,13 +11,16 @@ const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
 const {id} = event.queryStringParameters
 if(id){
   try{
-    const product = await airtable.retrieve(id);
+    let product = await airtable.retrieve(id);
     if(product.error){
       return {
         statusCode: 404,
         body: `No Product with an ID: ${id}` 
       }
     }
+    const {id} = product;
+    const {category, name, price, images, size, material, dimension, innerMaterial, weight, stock, instock} = product.fields
+    product = {id, category, name, price, images, size, material, dimension, innerMaterial, weight, stock, instock}
     return {
       headers:{
         'Access-Control-Allow-Origin': '*'
